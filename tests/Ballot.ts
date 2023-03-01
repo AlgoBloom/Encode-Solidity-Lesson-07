edfab083a46eeeb5139576d090f8e34ac7af73f5
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-01 06:34:23 -0700

    Before each working.

--- tests/Ballot.ts
+++ tests/Ballot.ts
@@ -1,13 +1,13 @@
 import { ethers } from "hardhat";
+import { Ballot } from "../typechain-types";
 
 describe("Ballot", function () {
     // defining var here allows us to use globally for all tests
-    let ballotContract
+    let ballotContract: Ballot;
     beforeEach(async function () {
         const ballotContractFactory = await ethers.getContractFactory("Ballot");
-        ballotContract = await ballotContractFactory.deploy();
-        await ballotContract.deployed();
-
+        ballotContract = await ballotContractFactory.deploy([ethers.utils.formatBytes32String("Alice"), ethers.utils.formatBytes32String("Bob"), ethers.utils.formatBytes32String("Barli")]);
+        console.log("It worked!")
     });
     describe("When the contract is deployed", function () {
         it("has the provided proposals", async function () {});
